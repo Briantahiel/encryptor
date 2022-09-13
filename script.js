85488df1a561483c14dde1dff75bbe20e11293e2
BrianTahiel <briang.2012@hotmail.com>
2022-09-13 02:09:45 -0300

    last details

--- script.js
+++ script.js
@@ -26,35 +26,39 @@ function encrypt(){
         messageError.innerHTML = "Ingrese un texto";
         messageError.style.animation = "shake .4s";
     }
-         
+     
 }
 
 function desencrypt(){
     text = textToChange.value;
     noAccent = text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    encryptText = text.toLowerCase().trim()
+    encryptText = noAccent.toLowerCase().trim()
     .replaceAll("enter", "e")
     .replaceAll("imes", "i")
     .replaceAll("ai", "a")
     .replaceAll("ober", "o")
     .replaceAll("ufat", "u")
+    if(encryptText !== ""){
+        backgroundImg.style.display = "none";
+        document.querySelector(".message").innerHTML = encryptText;
+        containerMessage.style.display = "block";
+        messageError.style.display = "none";
+    }
     if(encryptText == ""){
         messageError.style.display = "block";
         messageError.innerHTML = "Ingrese un texto";
         messageError.style.animation = "shake .4s";
-    }
-    
+    }  
     document.querySelector(".message").innerHTML = encryptText;
-}
-
-function del(){
-    textToChange.value = ""
-    document.querySelector(".message").innerHTML = "";
-    backgroundImg.style.display = "";
-    containerMessage.style.display = "none";
 }
 
 function copyText(){
     messageResult.select();
     navigator.clipboard.writeText(messageResult.value);
+}
+function del(){
+    textToChange.value = ""
+    document.querySelector(".message").innerHTML = "";
+    backgroundImg.style.display = "";
+    containerMessage.style.display = "none";
 }
